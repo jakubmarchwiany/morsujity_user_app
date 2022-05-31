e00f1433f074fd97dc8c504fdbc22aa01fd48baf
xkvbikx <50952542+xkvbikx@users.noreply.github.com>
2022-06-01 01:08:44 +0200

    added (page structure, footer),

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { Stack, ThemeProvider } from "@mui/material";
+
 import "./Assets/App.css";
 import Navbar from "./Layouts/Navbar";
+import { myTheme } from "./Assets/theme";
+import Footer from "./Layouts/Footer";
+import Navigator from "./Layouts/Navigator";
+import Content from "./Layouts/Content";
+import Ads from "./Layouts/Ads";
 
 function App() {
     return (
-        <div>
-            <Navbar />
-        </div>
+        <ThemeProvider theme={myTheme}>
+            <Stack sx={{minHeight: "100vh"}}>
+                <Navbar />
+                <Stack sx={{flex: 1}} direction="row" justifyContent="space-between">
+                    <Navigator />
+                    <Content />
+                    <Ads />
+                </Stack>
+                <Footer />
+            </Stack>
+        </ThemeProvider>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "@mui/material";
-
-import { myTheme } from "./Assets/theme";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -13,8 +10,6 @@ import "@fontsource/roboto/700.css";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={myTheme}>
-            <App />
-        </ThemeProvider>
+        <App />
     </React.StrictMode>
 );
