20c52df6e06cc60b0bcae7958526ac34128a535e
xkvbikx <xkvbikx@gmail.com>
2022-08-04 20:41:06 +0200

    +added Navigator

---
+++ src/Components/Main/GuestButtonList.tsx
@@ -0,0 +1,120 @@
+import {
+    Book,
+    ExpandLess,
+    ExpandMore,
+    GroupAdd,
+    HowToReg,
+    Info,
+    Login,
+    Storefront,
+    Web,
+} from "@mui/icons-material";
+import {
+    Collapse,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+interface Props {
+    logIn: boolean;
+    close?: () => void;
+}
+
+function GuestButtonList({ close }: Props) {
+    const [open, setOpen] = useState(true);
+
+    return (
+        <>
+            <ListItem disablePadding onClick={close}>
+                <ListItemButton component={Link} to="/">
+                    <ListItemIcon>
+                        <Web fontSize="large" />
+                    </ListItemIcon>
+                    <ListItemText
+                        primaryTypographyProps={{ letterSpacing: 1.5 }}
+                        primary="Strona główna"
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding onClick={close}>
+                <ListItemButton component={Link} to="/login">
+                    <ListItemIcon>
+                        <Login fontSize="large" />
+                    </ListItemIcon>
+                    <ListItemText
+                        primaryTypographyProps={{ letterSpacing: 1.5 }}
+                        primary="Logowanie"
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding onClick={close}>
+                <ListItemButton component={Link} to="/register">
+                    <ListItemIcon>
+                        <HowToReg fontSize="large" />
+                    </ListItemIcon>
+                    <ListItemText
+                        primaryTypographyProps={{ letterSpacing: 1.5 }}
+                        primary="Rejestracja"
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItemButton onClick={() => setOpen(!open)}>
+                <ListItemIcon>
+                    <Book fontSize="large" />
+                </ListItemIcon>
+                <ListItemText primaryTypographyProps={{ letterSpacing: 1.5 }} primary="Blog" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding onClick={close}>
+                    <ListItemButton sx={{ pl: 4 }} component={Link} to="/blog">
+                        <ListItemIcon style={{ minWidth: "40px" }}>
+                            <Info />
+                        </ListItemIcon>
+                        <ListItemText primary="O Aplikacja" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 4 }} component={Link} to="/blog">
+                        <ListItemIcon style={{ minWidth: "40px" }}>
+                            <Info />
+                        </ListItemIcon>
+                        <ListItemText primary="Morsowanie ?" />
+                    </ListItemButton>
+
+                    <ListItemButton sx={{ pl: 4 }} component={Link} to="/blog">
+                        <ListItemIcon style={{ minWidth: "40px" }}>
+                            <Info />
+                        </ListItemIcon>
+                        <ListItemText primary="+ / - Morsowania" />
+                    </ListItemButton>
+                </List>
+            </Collapse>
+            <ListItem disablePadding onClick={close}>
+                <ListItemButton component={Link} to="/search">
+                    <ListItemIcon>
+                        <GroupAdd fontSize="large" />
+                    </ListItemIcon>
+                    <ListItemText
+                        primaryTypographyProps={{ letterSpacing: 1.5 }}
+                        primary="Szukaj Grupy"
+                    />
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding onClick={close}>
+                <ListItemButton component={Link} to="/shop" disabled>
+                    <ListItemIcon>
+                        <Storefront fontSize="large" />
+                    </ListItemIcon>
+                    <ListItemText primaryTypographyProps={{ letterSpacing: 1.5 }} primary="Sklep" />
+                </ListItemButton>
+            </ListItem>
+        </>
+    );
+}
+
+export default GuestButtonList;
--- src/Layouts/Navigator.tsx
+++ src/Layouts/Navigator.tsx
@@ -1,162 +1,58 @@
-import {
-    AccountBox,
-    Article,
-    Group,
-    Home,
-    ModeNight,
-    Person,
-    Settings,
-    Storefront,
-    AddTask,
-    Logout,
-    Groups,
-    ExpandLess,
-    ExpandMore,
-    GroupAdd,
-} from "@mui/icons-material";
-import {
-    Box,
-    Collapse,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Switch,
-} from "@mui/material";
-import { useState } from "react";
+import { DarkMode, LightMode } from "@mui/icons-material";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, Switch } from "@mui/material";
+import GuestButtonList from "Components/Main/GuestButtonList";
+import UserButtonList from "Components/User/UserButtonList";
+import { useAppDispatch, useAppSelector } from "hooks";
+import { useNavigate } from "react-router-dom";
+import { appActions } from "Store/app-slice";
+import { logoutUserThunk } from "Store/user-actions";
 
 const Navigator = () => {
-    const [open, setOpen] = useState(true);
+    const user = useAppSelector((state) => state.user);
+    const mode = useAppSelector((state) => state.app.mode);
 
-    const handleClick = () => {
-        setOpen(!open);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const logoutHandler = () => {
+        dispatch(logoutUserThunk(user.token!, navigate));
+        window.localStorage.setItem("logout", Date.now().toString());
     };
 
     return (
-        <Box flex={2} sx={{ display: { xs: "none", md: "block" } }}>
-            <Box px={2} pt={2} width={"100%"}>
-                <List>
-                    <ListItem disablePadding>
-                        <ListItemButton component="a" href="#home">
-                            <ListItemIcon>
-                                <Home />
-                            </ListItemIcon>
-                            <ListItemText
-                                primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                primary="Strona Główna"
-                            />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component="a" href="#home">
-                            <ListItemIcon>
-                                <AddTask />
-                            </ListItemIcon>
-                            <ListItemText
-                                primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                primary="Dodaj Aktywność"
-                            />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItemButton onClick={handleClick}>
+        <Box
+            flex={2}
+            sx={{ display: { xs: "none", lg: "block" } }}
+            bgcolor={"background.default"}
+            color={"text.primary"}
+        >
+            <List sx={{ mt: 3 }}>
+                {user.logIn ? (
+                    <UserButtonList
+                        logIn={user.logIn}
+                        type={user.type!}
+                        userImage={user.image!}
+                        logoutHandler={logoutHandler}
+                    />
+                ) : (
+                    <GuestButtonList logIn={user.logIn} />
+                )}
+                <ListItem disablePadding>
+                    <ListItemButton>
                         <ListItemIcon>
-                            <Groups />
+                            {mode === "dark" ? (
+                                <DarkMode fontSize="large" />
+                            ) : (
+                                <LightMode fontSize="large" />
+                            )}
                         </ListItemIcon>
-                        <ListItemText
-                            primaryTypographyProps={{ letterSpacing: 1.5 }}
-                            primary="Twoje Grupy"
+                        <Switch
+                            checked={mode === "dark" ? true : false}
+                            onChange={() => dispatch(appActions.switchMode())}
                         />
-                        {open ? <ExpandLess /> : <ExpandMore />}
                     </ListItemButton>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <List component="div" disablePadding>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <ListItemIcon>
-                                    <Group />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                    primary="Hipotermia"
-                                />
-                            </ListItemButton>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <ListItemIcon>
-                                    <Group />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                    primary="Chełmskie Morsy"
-                                />
-                            </ListItemButton>
-                            <ListItemButton sx={{ pl: 4 }} component="a" href="#simple-list">
-                                <ListItemIcon>
-                                    <GroupAdd />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                    primary="Szukaj Grupy"
-                                />
-                            </ListItemButton>
-                        </List>
-                    </Collapse>
-                    <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list" disabled>
-                            <ListItemIcon>
-                                <Storefront />
-                            </ListItemIcon>
-                            <ListItemText
-                                primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                primary="Sklep"
-                            />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
-                            <ListItemIcon>
-                                <AccountBox />
-                            </ListItemIcon>
-                            <ListItemText
-                                primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                primary="Konto"
-                            />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
-                            <ListItemIcon>
-                                <Settings />
-                            </ListItemIcon>
-                            <ListItemText
-                                primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                primary="Ustawienia"
-                            />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
-                            <ListItemIcon>
-                                <Logout />
-                            </ListItemIcon>
-                            <ListItemText
-                                primaryTypographyProps={{ letterSpacing: 1.5 }}
-                                primary="Wyloguj"
-                            />
-                        </ListItemButton>
-                    </ListItem>
-
-                    {/* <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
-                            <ListItemIcon>
-                                <ModeNight />
-                            </ListItemIcon>
-                            <Switch
-                            // onChange={(e) => setMode(mode === "light" ? "dark" : "light")}
-                            />
-                        </ListItemButton>
-                    </ListItem> */}
-                </List>
-            </Box>
+                </ListItem>
+            </List>
         </Box>
     );
 };
