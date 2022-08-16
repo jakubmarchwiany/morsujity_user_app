b4ef0ccdca98b0d83d169f39df2d60d05dd81c04
xkvbikx <xkvbikx@gmail.com>
2022-08-17 00:01:34 +0200

    refractor navigator.tsx
    - add auto login method
    - add all navigate logic
    - optimize render (stopping useless re render )
    - add prefer dark mode method

--- src/Layouts/Navigator.tsx
+++ src/Layouts/Navigator.tsx
@@ -1,60 +1,87 @@
 import { DarkMode, LightMode } from "@mui/icons-material";
-import { Box, List, ListItem, ListItemButton, ListItemIcon, Switch } from "@mui/material";
+import {
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    Switch,
+    Theme,
+    useMediaQuery,
+    useTheme,
+} from "@mui/material";
 import GuestButtonList from "Components/Main/GuestButtonList";
 import UserButtonList from "Components/User/UserButtonList";
-import { useAppDispatch, useAppSelector } from "hooks";
+import { useAppDispatch, useAppSelector } from "hooks/redux";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { appActions } from "Store/app-slice";
-import { logoutUserThunk } from "Store/user-actions";
+import { logoutUser } from "Store/auth-actions";
+import { getUserData } from "Store/user-actions";
 
-const Navigator = () => {
+interface NavigatorProps {
+    navbar: boolean;
+    onClose?: () => void;
+}
+
+const Navigator = (props: NavigatorProps) => {
     const user = useAppSelector((state) => state.user);
     const mode = useAppSelector((state) => state.app.mode);
+
+    const theme = useTheme<Theme>();
+    let bigScreen = useMediaQuery(theme.breakpoints.up("lg"));
+
+    if (props.navbar) {
+        bigScreen = !bigScreen;
+    }
 
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
+    useEffect(() => {
+        if (!user.logIn && Cookies.get("Authorization")) {
+            console.log("siema");
+            dispatch(getUserData(navigate));
+        }
+        // eslint-disable-next-line
+    }, []);
+
     const logoutHandler = () => {
-        dispatch(logoutUserThunk(user.token!, navigate));
+        dispatch(logoutUser(navigate));
         window.localStorage.setItem("logout", Date.now().toString());
     };
 
-    return (
-        <Box
-            flex={2}
-            sx={{ display: { xs: "none", lg: "block" } }}
-            bgcolor={"background.default"}
-            color={"text.primary"}
-        >
-            <List sx={{ mt: 3 }}>
-                {user.logIn ? (
-                    <UserButtonList
-                        logIn={user.logIn}
-                        type={user.type!}
-                        userImage={user.image!}
-                        logoutHandler={logoutHandler}
+    return bigScreen ? (
+        <List>
+            {user.logIn ? (
+                <UserButtonList
+                    logIn={user.logIn}
+                    type={user.type!}
+                    userImage={user.image!}
+                    close={props.onClose}
+                    logoutHandler={logoutHandler}
+                />
+            ) : (
+                <GuestButtonList logIn={user.logIn} close={props.onClose} />
+            )}
+            <ListItem disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
+                        {mode === "dark" ? (
+                            <DarkMode fontSize="large" />
+                        ) : (
+                            <LightMode fontSize="large" />
+                        )}
+                    </ListItemIcon>
+                    <Switch
+                        checked={mode === "dark" ? true : false}
+                        onChange={() => dispatch(appActions.switchMode())}
                     />
-                ) : (
-                    <GuestButtonList logIn={user.logIn} />
-                )}
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            {mode === "dark" ? (
-                                <DarkMode fontSize="large" />
-                            ) : (
-                                <LightMode fontSize="large" />
-                            )}
-                        </ListItemIcon>
-                        <Switch
-                            checked={mode === "dark" ? true : false}
-                            onChange={() => dispatch(appActions.switchMode())}
-                        />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-        </Box>
+                </ListItemButton>
+            </ListItem>
+        </List>
+    ) : (
+        <></>
     );
 };
-
 export default Navigator;
