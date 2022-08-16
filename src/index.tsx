9762295cbe727723f163b78ebd89787cdda93446
xkvbikx <xkvbikx@gmail.com>
2022-08-16 23:39:05 +0200

    refractor and add validateEnv
    - Router -> BrowserRouter
    - move fontsource to assets\theme.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-
 import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import store from "Store/index";
+import App from "./App";
+import validateEnv from "./utils/validate-env";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+validateEnv();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
         <HelmetProvider>
             <Provider store={store}>
-                <Router>
+                <BrowserRouter>
                     <App />
-                </Router>
+                </BrowserRouter>
             </Provider>
         </HelmetProvider>
     </React.StrictMode>
