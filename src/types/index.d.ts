a9b4331f76756d4fc41c4cd721ec3d1299fd2043
xkvbikx <xkvbikx@gmail.com>
2022-08-16 23:45:49 +0200

    add validate env variables

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,14 +5,24 @@ declare module "*.jpg" {
 }
 
 declare global {
-    interface Window {
-        paypal: any;
+    namespace NodeJS {
+        interface ProcessEnv {
+            REACT_APP_ENV: "development" | "production";
+            REACT_APP_DEV_API_ENDPOINT: string;
+            REACT_APP_PRO_API_ENDPOINT: string;
+        }
     }
 }
 
-declare module "react" {
-    interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-        // extends React's HTMLAttributes
-        crossorigin?: string;
-    }
-}
+// declare global {
+//     interface Window {
+//         paypal: any;
+//     }
+// }
+
+// declare module "react" {
+//     interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+//         // extends React's HTMLAttributes
+//         crossorigin?: string;
+//     }
+// }
---
+++ src/utils/validate-env.ts
@@ -0,0 +1,10 @@
+import { cleanEnv, str } from "envalid";
+
+function validateEnv() {
+    cleanEnv(process.env, {
+        REACT_APP_ENV: str({ choices: ["development", "production"] }),
+        REACT_APP_DEV_API_ENDPOINT: str(),
+        REACT_APP_PRO_API_ENDPOINT: str(),
+    });
+}
+export default validateEnv;
