cac506562277ab18176706fbd09ccb24a161722a
xkvbikx <xkvbikx@gmail.com>
2023-02-14 23:17:16 +0100

    add last activity panel

--- src/components/user/Dashboard.tsx
+++ src/components/user/Dashboard.tsx
@@ -1,19 +1,85 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Container, Stack, Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Unstable_Grid2";
+import dayjs from "dayjs";
+import "dayjs/locale/pl";
+import relativeTime from "dayjs/plugin/relativeTime"; // import plugin
+import { useAppSelector } from "hooks/redux";
+import { displayTime } from "utils/useful";
+
+dayjs.extend(relativeTime);
+dayjs.locale("pl");
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.background.default,
+    ...theme.typography.body2,
+    padding: theme.spacing(3),
+    color: theme.palette.text.secondary,
+    borderRadius: 25,
+}));
 
 function DashBoard() {
+    const statisics = useAppSelector((state) => state.user.statistics);
+
+    const generateLastActivities = () => {
+        let newArray = [...statisics!.activity];
+        return newArray
+            .sort(function (a, b) {
+                const dateA = new Date(a.date!);
+                const dateB = new Date(b.date!);
+                return dateB.getTime() - dateA.getTime();
+            })
+            .slice(0, 3)
+            .map((activity) => {
+                return (
+                    <Stack
+                        direction='row'
+                        p={1}
+                        boxShadow={5}
+                        mt={1}
+                        justifyContent='space-between'
+                        borderRadius={2}
+                        key={activity._id}
+                    >
+                        <Typography>{activity.isMors ? "mors" : "zimny prysznic"}</Typography>
+                        <Typography>{displayTime(activity.duration)}</Typography>
+                        <Typography>{dayjs(activity.date).from(dayjs())}</Typography>
+                    </Stack>
+                );
+            });
+    };
+
     return (
         <Container
             component='main'
             sx={{
-                px: { xs: 0, sm: 10, md: 5, lg: 15, xl: 30 },
+                px: { xs: 0, sm: 10, md: 5, lg: 15, xl: 20 },
             }}
         >
-            <Box textAlign='center'>
-                <Typography variant='h1'>Morsuj i Ty</Typography>
-                <Typography mt={10} mb={5} variant='h5'>
-                    Strona użytkownika
-                </Typography>
-            </Box>
+            <Grid container spacing={2}>
+                <Grid xs={6}>
+                    <Item>
+                        <Typography variant='h5' mb={3}>
+                            Ostatnie aktywności
+                        </Typography>
+                        {statisics?.activity.length === 0 ? (
+                            <Typography>Brak akywności, na co czekasz :)</Typography>
+                        ) : (
+                            generateLastActivities()
+                        )}
+                    </Item>
+                </Grid>
+                <Grid xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid xs={8}>
+                    <Item>xs=8</Item>
+                </Grid>
+            </Grid>
         </Container>
     );
 }
