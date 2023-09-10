8b59db541d7129258cb348f1d9c954c753b5d32c
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-09-10 08:52:31 +0200

    change port

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        port: 3000,
+        port: 4000,
     },
     build: {
         outDir: "./build",
